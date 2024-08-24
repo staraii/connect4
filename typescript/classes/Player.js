7c38e5faf98035894735b27f606eb2cb130316c9
Staffan Björk <staffanbjork86@gmail.com>
2024-08-24 18:27:06 +0200

    Player class

---
+++ typescript/classes/Player.js
@@ -0,0 +1,9 @@
+import prompt from "../helpers/prompt.js";
+
+export default class Player {
+  constructor(color, playerType) {
+    this._name = name;
+    this.color = color;
+    this.playerType = playerType;
+  }
+}
