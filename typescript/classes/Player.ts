0704df0e04c86c37f4b8e8775ac10d518f3447e2
Staffan Björk <staffanbjork86@gmail.com>
2024-08-24 21:31:19 +0200

    Player class - constructor modified

--- typescript/classes/Player.ts
+++ typescript/classes/Player.ts
@@ -3,19 +3,19 @@ import prompt from "../helpers/prompt.js";
 export default class Player {
   _name: string;
   color: string;
-  playerType: string;
+  _playerType: string | undefined;
 
-  constructor(color: string, playerType: string) {
+  constructor(color: string) {
     this._name = "";
     this.color = color;
-    this.playerType = playerType;
+    this._playerType = undefined;
   }
 
   set name(name: string) {
     if (name.trim().length > 0) {
       this._name = name;
     } else {
-      throw new Error("Name must contain at least one character.");
+      throw new Error("Player name must contain at least one character.");
     }
   }
 
