ac875345d223ecbbaf7795b7acda7f53f28b4318
Staffan Björk <staffanbjork86@gmail.com>
2024-08-28 20:42:11 +0200

    Board class - render method added

--- typescript/classes/Board.ts
+++ typescript/classes/Board.ts
@@ -7,7 +7,15 @@ export default class Board {
   constructor() {
     this.rows = 6;
     this.cols = 7;
-    this.matrix = [...new Array(this.rows)].map(_row => [...new Array(this.cols)].map(_column => " "));
+    this.matrix = [...new Array(this.rows)].map((_row) => [...new Array(this.cols)].map((_column) => " "));
   }
 
+  render() {
+		let line = "\n" + "-".repeat(this.cols * 4 + 1) + "\n";
+		let lastLine = "\n" + "-".repeat(this.cols * 4 + 1);
+    let columnNumbers = [...new Array(7)].map((_col, index) => `${index + 1}`);
+    
+    console.log(line + this.matrix.map((row) => row.map((col) => `| ${col === null ? " " : col} `).join("") + "|").join(line) + lastLine);
+    console.log(columnNumbers.map((col) => `| ${col} `).join("") + "|" + "\n");
+  }
 }
