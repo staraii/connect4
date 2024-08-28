5fcb76582783ce7fdaad7ecc0ab8fad9baca483d
Staffan Björk <staffanbjork86@gmail.com>
2024-08-28 13:04:13 +0200

    GameSetup class - reworked setter, paramaters

--- typescript/classes/GameSetup.ts
+++ typescript/classes/GameSetup.ts
@@ -1,52 +1,32 @@
-import Player from "./Player.js";
+//import Player from "./Player.js";
 import prompt from "../helpers/prompt.js";
+import { GameType } from "../types/types.js";
 
 export default class GameSetup {
-  _gameType: string;
-  _gameTypesList: string[];
-  _playerOne: Player;
-  _playerTwo: Player;
+  #gameType: string = "";
+  //_playerOne: Player;
+  //_playerTwo: Player;
 
   constructor() {
-    this._gameType = undefined!;
-    this._gameTypesList = ["1 vs 1", "1 vs Computer", "Computer vs Computer"];
-    this._playerOne = undefined!;
-    this._playerTwo = undefined!;
-    this.selectGameType();
-    //this.setPlayers();
+    this.gameType = "";
+    //this._playerOne = undefined!;
+    //this._playerTwo = undefined!;
   }
 
   set gameType(option: string) {
-    let index = +option;
-    if (index > 0 && index < 4) {
-      this._gameType = this._gameTypesList[index -1]
-    } else {
-      throw new Error("Please select a gametype between 1-3.");
+    function validateInput(input: string) {
+      return /^[1-3]$/.test(input);
     }
+    let valid = validateInput(option);
+    while (!valid) {
+      console.log("1. Dual player \n2. Single player \n3. AI \n");
+      option = prompt(`Select game type (1-3): `);
+      validateInput(option) ? valid = true : (console.clear(), console.log("Invalid input, please try again."));
+    }
+    this.#gameType = GameType[+option];
   }
   get gameType() {
-    return this._gameType;
+    return this.#gameType;
   }
-  selectGameType() {
-    let valid = false;
-    while(!valid){
-      try {
-        let gameType = prompt(`Select game type. \n 1. ${this._gameTypesList[0]}  \n 2. ${this._gameTypesList[1]} \n 3. ${this._gameTypesList[2]}`);
-        this.gameType = gameType;
-        valid = true;
-      } catch (error) {
-        let message;
-        if (error instanceof Error) {
-          message = error.message;
-        } else {
-          message = String(error);
-        }
-        console.log(message);
-      } 
-    }
-  }
-  // setPlayers() {
-    
-  // }
 } 
 
