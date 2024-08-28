fd11c049195930d5a70ce2e37a2bdb58de0c1c54
Staffan Björk <staffanbjork86@gmail.com>
2024-08-28 17:55:40 +0200

    Game class - player properties added

--- typescript/classes/Game.ts
+++ typescript/classes/Game.ts
@@ -1,8 +1,12 @@
 import GameSetup from "./GameSetup.js";
-
+import { GamePlayer } from "../types/types.js";
 export default class Game {
   setup: GameSetup;
+  playerOne: GamePlayer;
+  playerTwo: GamePlayer;
   constructor() {
     this.setup = new GameSetup();
+    this.playerOne = this.setup.playerOne;
+    this.playerTwo = this.setup.playerTwo;
   }
 }
