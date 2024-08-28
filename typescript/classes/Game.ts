c0c5c334d5ca8b3ca73cbb1489a1188f5aba81c0
Staffan Björk <staffanbjork86@gmail.com>
2024-08-28 21:07:34 +0200

    Game class - Board and CurrentGame props added

--- typescript/classes/Game.ts
+++ typescript/classes/Game.ts
@@ -1,12 +1,18 @@
 import GameSetup from "./GameSetup.js";
 import { GamePlayer } from "../types/types.js";
+import Board from "./Board.js";
+import CurrentGame from "./CurrentGame.js";
 export default class Game {
   setup: GameSetup;
   playerOne: GamePlayer;
   playerTwo: GamePlayer;
+  board: Board;
+  currentGame: CurrentGame;
   constructor() {
     this.setup = new GameSetup();
     this.playerOne = this.setup.playerOne;
     this.playerTwo = this.setup.playerTwo;
+    this.board = new Board();
+    this.currentGame = new CurrentGame();
   }
 }
