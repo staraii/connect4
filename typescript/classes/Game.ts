6f4fbec7b92950290cd08ed3c5417fba438b84d2
Staffan Björk <staffanbjork86@gmail.com>
2024-09-01 13:47:17 +0200

    Game class - initialization of Board removed

--- typescript/classes/Game.ts
+++ typescript/classes/Game.ts
@@ -1,20 +1,17 @@
 import GameSetup from "./GameSetup.js";
 import { GamePlayer } from "../types/types.js";
-import Board from "./Board.js";
 import CurrentGame from "./CurrentGame.js";
 
 export default class Game {
   setup: GameSetup;
   playerOne: GamePlayer;
   playerTwo: GamePlayer;
-  board: Board;
   currentGame: CurrentGame;
 
   constructor() {
     this.setup = new GameSetup();
     this.playerOne = this.setup.playerOne;
     this.playerTwo = this.setup.playerTwo;
-    this.board = new Board();
-    this.currentGame = new CurrentGame(this.board, this.playerOne, this.playerTwo);
+    this.currentGame = new CurrentGame(this.playerOne, this.playerTwo);
   }
 }
