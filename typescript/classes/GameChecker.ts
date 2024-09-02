b2442fac1cd3a74ce17242970213e7f10bfcb653
Staffan Björk <staffanbjork86@gmail.com>
2024-09-02 10:29:25 +0200

    GameChecker class - checkForWin method added

--- typescript/classes/GameChecker.ts
+++ typescript/classes/GameChecker.ts
@@ -1,13 +1,25 @@
-import { Matrix, Move } from "../types/types.js";
+import { Matrix, Move, Color } from "../types/types.js";
 
 export default class GameChecker {
   isGameOver: boolean;
   isDraw: boolean;
-  isWinner: string | undefined;
+  isWinner: number | undefined;
 
   constructor() {
     this.isGameOver = false;
     this.isDraw = false;
     this.isWinner = undefined;
+  }
+  checkForWin(board: Matrix, lastMove: Move, movesMade: number, currentPlayer: number) {
+    const player = Color[currentPlayer]
+    
+    if (this.checkHorizontal(board, lastMove, player) || this.checkVertical(board, lastMove, player) || this.checkDiagonal(board, lastMove, player)) {
+      this.isWinner = currentPlayer;
+      this.isGameOver = true;
+    }
+    if (movesMade >= 42 && !this.isWinner) {
+      this.isDraw = true;
+      this.isGameOver = true;
+    }
   }
 }
