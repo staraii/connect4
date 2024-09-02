a532529ef165121703386338901ce42b70ed3f56
Staffan Björk <staffanbjork86@gmail.com>
2024-09-02 10:26:13 +0200

    Game class - player props removed

--- typescript/classes/Game.ts
+++ typescript/classes/Game.ts
@@ -1,17 +1,12 @@
 import GameSetup from "./GameSetup.js";
-import { GamePlayer } from "../types/types.js";
 import CurrentGame from "./CurrentGame.js";
 
 export default class Game {
   setup: GameSetup;
-  playerOne: GamePlayer;
-  playerTwo: GamePlayer;
   currentGame: CurrentGame;
 
   constructor() {
     this.setup = new GameSetup();
-    this.playerOne = this.setup.playerOne;
-    this.playerTwo = this.setup.playerTwo;
-    this.currentGame = new CurrentGame(this.playerOne, this.playerTwo);
+    this.currentGame = new CurrentGame(this.setup.playerOne, this.setup.playerTwo);
   }
 }
