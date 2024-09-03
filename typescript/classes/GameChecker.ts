24c2f862a30378131a9f40f7b198ac583e1e27a8
Staffan Björk <staffanbjork86@gmail.com>
2024-09-03 13:48:39 +0200

    GameChecker class - checkBoard method added, other methods remover

--- typescript/classes/GameChecker.ts
+++ typescript/classes/GameChecker.ts
@@ -1,4 +1,4 @@
-import { Matrix, Move, Color, BoardProps } from "../types/types.js";
+import { Matrix, Move, Color } from "../types/types.js";
 
 export default class GameChecker {
   isGameOver: boolean;
@@ -17,11 +17,8 @@ export default class GameChecker {
     currentPlayer: number
   ) {
     const player = Color[currentPlayer];
-
     if (
-      this.checkHorizontal(board, lastMove, player) ||
-      this.checkVertical(board, lastMove, player) ||
-      this.checkDiagonal(board, lastMove, player)
+      this.checkBoard(board, lastMove, player)
     ) {
       this.isWinner = currentPlayer;
       this.isGameOver = true;
@@ -31,25 +28,48 @@ export default class GameChecker {
       this.isGameOver = true;
     }
   }
-  checkHorizontal(board: Matrix, lastMove: Move, player: string) {
-    for (let cols = 0; cols <= BoardProps.Cols - 4; cols++) {
-      if (
-        board[lastMove.row].slice(cols, cols + 4).every((col) => col === player)
-      ) {
+  checkBoard(board: Matrix, lastMove: Move, player: string) {
+    const diagonalOffsets = [
+      [lastMove.row - 3, lastMove.col - 3],
+      [lastMove.row - 3, lastMove.col + 3],
+    ];
+    for (let i = 0; i < 4; i++){
+      const horizontal = board[lastMove.row].slice(i, i + 4).every(col => col === player);
+      let vertical = false;
+      if (i < 3) {
+        vertical = [0, 1, 2, 3].every((row) => board[i + row][lastMove.col] === player);
+      }
+      if (horizontal || vertical) {
         return true;
       }
-    }
-  }
-  checkVertical(board: Matrix, lastMove: Move, player: string) {
-    const result: number = board.reduce((count, row) => {
-      if (row[lastMove.col] === player) {
-        return count + 1;
-      } else {
-        return 0;
+      let diagonal = 0;
+      let roOne = diagonalOffsets[0][0] + i;
+      let coOne = diagonalOffsets[0][1] + i;
+      let roTwo = diagonalOffsets[1][0] + i;
+      let coTwo = diagonalOffsets[1][1] - i;
+      if (!(roOne < 0 || roOne > 2 || coOne < 0 || coOne > 3)) {
+        for (let j = 0; j < 4; j++){
+          if (board[roOne + j][coOne + j] === player) {
+            diagonal++
+          }
+        }
+        if (diagonal === 4) {
+          return true;
+        }
+        diagonal = 0;
       }
-    }, 0);
-    if (result >= 4) {
-      return true;
+      if (!(roTwo < 0 || roTwo > 2 || coTwo < 0 || coTwo > 3)) {
+        for (let j = 0; j < 4; j++){
+          if (board[roTwo + j][coTwo - j] === player) {
+            diagonal++
+          }
+        }
+        if (diagonal === 4) {
+          return true;
+        }
+        diagonal = 0;
+      }
     }
+    return false;
   }
 }
