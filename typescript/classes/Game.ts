24bf262817029f76d371813db994932d8935f79b
Staffan Björk <staffanbjork86@gmail.com>
2024-09-03 14:09:16 +0200

    Game class - playAgain method added

--- typescript/classes/Game.ts
+++ typescript/classes/Game.ts
@@ -1,5 +1,7 @@
 import GameSetup from "./GameSetup.js";
 import CurrentGame from "./CurrentGame.js";
+import Input from "./Input.js";
+import { RegExes } from "../types/types.js";
 
 export default class Game {
   setup: GameSetup;
@@ -7,6 +9,21 @@ export default class Game {
 
   constructor() {
     this.setup = new GameSetup();
-    this.currentGame = new CurrentGame(this.setup.playerOne, this.setup.playerTwo);
+    this.currentGame = new CurrentGame(
+      this.setup.playerOne,
+      this.setup.playerTwo
+    );
+    this.playAgain();
+  }
+
+  playAgain() {
+    const playAgain = Input.getValid("Play again? (Y/N)", "Invalid alternative! Please enter Y (Yes) or N (No) \n", RegExes.PlayAgain);
+    if (playAgain.toLowerCase() === "n") {
+      return;
+    }
+    if (playAgain.toLowerCase() === "y") {
+      this.setup = new GameSetup();
+      this.currentGame = new CurrentGame(this.setup.playerOne, this.setup.playerTwo);
+    }
   }
 }
